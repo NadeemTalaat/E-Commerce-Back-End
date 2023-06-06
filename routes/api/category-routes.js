93bc15d2baae10214f26ef99ed536802d898c908
NadeemTalaat <nadeem.talaat@gmail.com>
2023-06-05 23:17:23 -0400

    Fixed GET methods in ./routes/api/category-routes.js file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,12 +16,17 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Category.findAll().then((categoryData) => {
-    res.json(categoryData);
-  });
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post("/", (req, res) => {
