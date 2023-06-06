5c8bf5f989b7a891323420127d044a717546f62e
NadeemTalaat <nadeem.talaat@gmail.com>
2023-06-05 23:11:56 -0400

    Defined all Sequelize associations in ./models/index.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,23 @@ const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
-  Category.findAll().then((categoryData) => {
-    res.json(categoryData);
-  });
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Categeory.findAll().then((categoryData) => {
+  Category.findAll().then((categoryData) => {
     res.json(categoryData);
   });
 });
